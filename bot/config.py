d69ebf5a0af04eb9324976f6ae0224842f0da25f
Noob <101978891+ZxLbni@users.noreply.github.com>
2024-10-01 19:01:57 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -13,7 +13,7 @@ class Telegram:
 class Server:
     BASE_URL = env.get("BASE_URL", "https://noob-file-strr-9c23f91496f7.herokuapp.com")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
-    PORT = int(env.get("PORT", 8080))
+    PORT = int(env.get("PORT", 3000))
 
 # LOGGING CONFIGURATION
 LOGGER_CONFIG_JSON = {
