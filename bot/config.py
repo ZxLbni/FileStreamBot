24d1b9c86918178fc093f6d4cc7d37f9fe2f5979
Noob <101978891+ZxLbni@users.noreply.github.com>
2024-10-12 16:32:19 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,13 +5,13 @@ class Telegram:
     API_HASH = env.get("TELEGRAM_API_HASH", "34982b52c4a83c2af3ce8f4fe12fe4e1")
     OWNER_ID = int(env.get("OWNER_ID", "6742022802"))
     ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "FILETOLINK5_ROBOT")
-    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "7414077653:AAGW_OeMNXxXfZMoDrCf-03RzGJdqVmSYTU")
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "HHTGFilezDLBot")
+    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "5411884013:AAG0nUmVlb5qDsP3UBHI7CyrcJ9iFB_Js3M")
     CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", "-1001907672237"))
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 4))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "https://stream.jooker.live")
+    BASE_URL = env.get("BASE_URL", "")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 3000))
 
